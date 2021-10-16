8638c23da788de8fff7cedd9e39ae3905ceaaf1b
MAFS <martin117h@gmail.com>
2021-10-16 16:25:14 -0500

    se termina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
 
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: environment.server, options: {} };
 
 
 @NgModule({
